f8a8684aeed8ed1bd5ec37d0862937fdde2ad67e
chris <noringc@gmail.com>
2025-05-13 18:28:56 +0000

    adding llm to solution, not done yet, needs mcp client code as well

---
+++ 03-GettingStarted/03-llm-client/solution/dotnet/Program.cs
@@ -0,0 +1,42 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Azure.Identity;
+
+var endpoint = "https://models.inference.ai.azure.com";
+var token = Environment.GetEnvironmentVariable("GITHUB_TOKEN"); // Your GitHub Access Token
+var client = new ChatCompletionsClient(new Uri(endpoint), new AzureKeyCredential(token));
+var chatHistory = new List<ChatRequestMessage>
+{
+    new ChatRequestSystemMessage("You are a helpful assistant that knows about AI")
+};
+
+while (true)
+{
+    Console.Write("You: ");
+    var userMessage = Console.ReadLine();
+
+    // Exit loop
+    if (userMessage.StartsWith("/q"))
+    {
+        break;
+    }
+
+    chatHistory.Add(new ChatRequestUserMessage(userMessage));
+    var options = new ChatCompletionsOptions(chatHistory)
+    {
+        Model = "Phi-3-medium-4k-instruct"
+    };
+
+    ChatCompletions? response = await client.CompleteAsync(options);
+    var content = response.Content;
+    Console.WriteLine($"Assistant response: {content}");
+    
+
+}
+
+// TODO:
+//   list tools on mcp server
+//   convert tool call to a tool on llm
+//   do user prompt
+//   verify if it was a function call
+//   call the right tool on the mcp server
---
+++ 03-GettingStarted/03-llm-client/solution/dotnet/README.md
@@ -0,0 +1,15 @@
+# Run this sample
+
+## Install libraries
+
+```sh
+dotnet add package Azure.AI.Inference --version 1.0.0-beta.4
+dotnet add package Azure.Identity --version 1.14.0-beta.4
+```
+
+## Run
+
+```sh
+dotnet run
+
+```
