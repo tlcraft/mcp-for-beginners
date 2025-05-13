bff18d6fe6b9292d6064803b2abfea52d59a1a80
chris <noringc@gmail.com>
2025-05-13 21:13:04 +0000

    adding function calling support, still wip for ch3, needs client

--- 03-GettingStarted/03-llm-client/solution/dotnet/Program.cs
+++ 03-GettingStarted/03-llm-client/solution/dotnet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Azure;
 using Azure.AI.Inference;
 using Azure.Identity;
+using System.Text.Json;
 
 var endpoint = "https://models.inference.ai.azure.com";
 var token = Environment.GetEnvironmentVariable("GITHUB_TOKEN"); // Your GitHub Access Token
@@ -22,19 +23,69 @@ while (true)
     }
 
     chatHistory.Add(new ChatRequestUserMessage(userMessage));
+
+    // define tools
+    FunctionDefinition addFunction = new FunctionDefinition("add")
+    {
+        Description = "adds two numbers",
+        Parameters = BinaryData.FromObjectAsJson(new
+        {
+            Type = "object",
+            Properties = new
+            {
+                a = new
+                {
+                    Type = "integer",
+                    Description = "the first number to add"
+                },
+                b = new
+                {
+                    Type = "integer",
+                    Description = "the second number to add"
+                }
+            }
+        },
+        new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase })
+    };
+
+
+    ChatCompletionsToolDefinition def = new ChatCompletionsToolDefinition(addFunction);
+
+    /*
+    tool choice requires --enable-auto-tool-choice and --tool-call-parser to be set","type"
+    */
+
+    // define options
     var options = new ChatCompletionsOptions(chatHistory)
     {
-        Model = "Phi-3-medium-4k-instruct"
+        Model = "gpt-4o-mini",
+        Tools = { def }
     };
+
+    // call the model  
 
     ChatCompletions? response = await client.CompleteAsync(options);
     var content = response.Content;
+
+
+    ChatCompletionsToolCall? calls = response.ToolCalls.FirstOrDefault();
+    for (int i = 0; i < response.ToolCalls.Count; i++)
+    {
+        var call = response.ToolCalls[i];
+        Console.WriteLine($"Tool call {i}: {call.Name} with arguments {call.Arguments}");
+        //it works!!, Tool call 0: add with arguments {"a":2,"b":4}
+    }
+
+
     Console.WriteLine($"Assistant response: {content}");
-    
+    // Console.WriteLine($"Function call: {functionCall?.Name}");
+
+    // check if tool call, if so, call the tool
 
 }
 
 // TODO:
+//   get a function call to work on the llm - CHECK
 //   list tools on mcp server
 //   convert tool call to a tool on llm
 //   do user prompt
